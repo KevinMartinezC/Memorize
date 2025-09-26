3af2ec6d8ca8c3fec6ba2dc8a296b336da1d0131
KevinMartinezC <ceronk003@gmail.com>
2025-09-26 12:03:05 -0600

    feat: add funtionality to add and remove cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,29 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻", "🎃", "🕷️", "👺","😳", "🧟‍♀️", "🧟‍♂️", "🎭", "🎨", "👩‍🎨", "👽", "🤯"]
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            cards
+            cardCountAdjusters
         }
-        .foregroundStyle(Color.orange)
         .padding()
         
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(Font.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button( action: {
+                cardCount += offset
+            
+        },label :{
+            Image(systemName:symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    
+    }
+    var cards: some View {
+        HStack{
+            ForEach(0..<cardCount, id: \.self){index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundStyle(.orange)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
 
 struct CardView: View {
+    let content: String
    @State var isFaceUp: Bool = true
     var body: some View {
         ZStack {
-            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp{
                 base.foregroundStyle(Color.white)
                 base.strokeBorder(style:StrokeStyle(lineWidth: 2))
-                Text("👻")
+                Text(content)
             }else{
                 base
             }
